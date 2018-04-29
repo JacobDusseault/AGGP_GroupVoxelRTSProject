9dfd5eb0de6f8b0e26e737878cd06c24525fe0ee
Raus <bloxxer14@gmail.com>
2018-04-29 19:28:47 -0400

    Added `Actions`
    These basically describe queueing things such as unit spawning and upgrades.

---
+++ Assets/Scripts/Action.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Action
+{
+	private string _name;
+	private float _duration;
+
+	public Action(string name, float duration)
+	{
+		_name = name;
+		_duration = duration;
+	}
+
+	public string GetName()
+	{
+		return _name;
+	}
+
+	public float GetDuration()
+	{
+		return _duration;
+	}
+}
