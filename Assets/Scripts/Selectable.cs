ae0f277fb5e521bfef58b904e184fdc28a73725b
Raus <bloxxer14@gmail.com>
2018-04-29 19:29:05 -0400

    Added `Selectable`
    Units and buildings are selectables

---
+++ Assets/Scripts/Selectable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selectable : MonoBehaviour
+{
+	public enum Team { Neutral, Red, Blue };
+
+	public Team team = Team.Neutral;
+
+	protected bool _selected = false;
+
+	public void Select()
+	{
+		_selected = true;
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+
+	public void Deselect()
+	{
+		_selected = false;
+		GetComponent<Renderer>().material.color = new Color(1f, 0.5f, 0.5f);
+	}
+}
