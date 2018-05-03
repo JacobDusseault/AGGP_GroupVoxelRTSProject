261f267fd899277108a91794ab9f26f894900d4d
Raus <bloxxer14@gmail.com>
2018-05-03 14:08:19 -0400

    ROCKS

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+	[SerializeField] private float _life = 5f;
+	private bool _active = true;
+
+	void Start()
+	{
+		Destroy(gameObject, _life);
+	}
+
+	void Update()
+	{
+
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+
+	}
+}
